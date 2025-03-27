a490d939345803dee582a1078f98e5c604ff425a
Viacheslav Verchonov <vverchonov@gmail.com>
2025-03-27 11:22:09 -0400

    metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,29 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Cherry Lips showbar',
   description: 'Ексклюзивний Нічний клуб, Стриптиз клуб та Кабаре в Ужгороді',
+  metadataBase: new URL('https://www.cherrylips.com.ua'),
+  openGraph: {
+    title: 'Cherry Lips showbar',
+    description: 'Ексклюзивний Нічний клуб, Стриптиз клуб та Кабаре в Ужгороді',
+    url: 'https://www.cherrylips.com.ua',
+    siteName: 'Cherry Lips showbar',
+    images: [
+      {
+        url: '/logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Cherry Lips showbar Logo',
+      },
+    ],
+    locale: 'uk_UA',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cherry Lips showbar',
+    description: 'Ексклюзивний Нічний клуб, Стриптиз клуб та Кабаре в Ужгороді',
+    images: ['/logo.png'],
+  },
   icons: {
     icon: [
       { url: '/favicon_io/favicon.ico' },
