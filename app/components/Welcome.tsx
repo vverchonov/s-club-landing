1de3d85a21a180c0514bb63997daf851c180e293
Viacheslav Verchonov <vverchonov@gmail.com>
2025-04-03 20:17:23 -0400

    welcome + link updated

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -97,7 +97,7 @@ const Welcome = () => {
                     <svg className="w-6 h-6 text-red-500 mr-4 flex-shrink-0 mt-1" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                     </svg>
-                    <span className="text-lg">Захоплеві шоу - це поєднання пластики, пристрасті і мистецтво спокушання. Граціозні рухи, ефектні трюки на пілоні - все це заворожує і приковує погляд.</span>
+                    <span className="text-lg">Захопливі шоу - це поєднання пластики, пристрасті і мистецтво спокушання. Граціозні рухи, ефектні трюки на пілоні - все це заворожує і приковує погляд.</span>
                   </li>
                   <li className="flex items-start">
                     <svg className="w-6 h-6 text-red-500 mr-4 flex-shrink-0 mt-1" fill="currentColor" viewBox="0 0 20 20">
