7f8c8965d17c8835662ad0c09b7561828c2131ea
Viacheslav Verchonov <vverchonov@gmail.com>
2025-09-10 14:50:10 -0400

    update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from '../../lib/context/TranslationContext'
 
 const Footer = () => {
   const { t } = useTranslation()
-  
+
   return (
     <footer className="bg-black text-white pt-16 pb-8">
       <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-12">
@@ -124,24 +124,6 @@ const Footer = () => {
                 <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"/>
               </svg>
             </Link> */}
-          </div>
-        </div>
-      </div>
-
-      {/* Bottom Bar */}
-      <div className="max-w-6xl mx-auto px-4 mt-16 pt-8 border-t border-gray-800">
-        <div className="flex flex-col md:flex-row justify-between items-center text-sm text-gray-400">
-          <div className="mb-4 md:mb-0">
-            {t.footer.copyright}
-          </div>
-          <div className="flex space-x-4">
-            <Link href="/imprint" className="hover:text-white transition-colors">
-              {t.footer.legal}
-            </Link>
-            <span>|</span>
-            <Link href="/privacy" className="hover:text-white transition-colors">
-              {t.footer.privacy}
-            </Link>
           </div>
         </div>
       </div>
