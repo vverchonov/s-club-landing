d075eb426e15ef3032aed20e24026fbbc7277710
Viacheslav Verchonov <vverchonov@gmail.com>
2025-09-11 16:59:16 -0400

    updated texts

--- lib/translations/en.ts
+++ lib/translations/en.ts
@@ -35,7 +35,7 @@ export const en = {
       workingHours: 'OPENING HOURS',
       phone: 'PHONE',
       email: 'EMAIL',
-      openingSoon: 'Opening soon',
+      openingSoon: 'Opening 19.09 from 20:30-5:00',
     }
   },
 
@@ -89,7 +89,7 @@ export const en = {
     workingHours: 'Opening Hours',
     phone: 'Phone',
     email: 'Email',
-    openingSoon: 'Opening soon',
+    openingSoon: 'Opening 19.09 from 20:30-5:00',
     form: {
       name: 'Name',
       namePlaceholder: 'Your name',
@@ -181,7 +181,7 @@ export const en = {
   footer: {
     title: 'Cherry Lips showbar Uzhgorod',
     hours: 'Opening Hours',
-    openingSoon: 'Opening soon',
+    openingSoon: 'Opening 19.09 from 20:30-5:00',
     sitemap: 'Sitemap',
     sitemapItems: {
       home: 'Home',
